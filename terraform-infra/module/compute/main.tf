e4e64fb72d38468c288f865a73eee898f988e1f6
Shashwat Singh <shashwatsinghrajput@outlook.com>
2026-06-20 20:27:45 +0530

    Adding sku size

--- terraform-infra/module/compute/main.tf
+++ terraform-infra/module/compute/main.tf
@@ -3,6 +3,7 @@ resource "azurerm_public_ip" "public_ip" {
   resource_group_name = var.resource_group_name
   location            = var.location
   allocation_method   = "Static"
+  sku = "Standard"
 }
 
 resource "azurerm_network_interface" "nic" {
