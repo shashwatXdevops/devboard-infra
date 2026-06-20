d406af8b96529a44ac5eeb49871bf831726d004f
Shashwat Singh <shashwatsinghrajput@outlook.com>
2026-06-20 22:06:44 +0530

    Added remote state

--- terraform-infra/providers.tf
+++ terraform-infra/providers.tf
@@ -5,8 +5,14 @@ terraform {
       version = "~> 3.7"
     }
   }
+  # Tell Terraform to use the vault we just created!
+  backend "azurerm" {
+    resource_group_name  = "tfstate-rg"
+    storage_account_name = "tfstate998877" 
+    container_name       = "tfstate"
+    key                  = "prod.terraform.tfstate"
+  }
 }
-
 provider "azurerm" {
   features {}
 }
