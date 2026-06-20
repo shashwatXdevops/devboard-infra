be32db49ee63dddde45c06803b0f88641b687399
Shashwat Singh <shashwatsinghrajput@outlook.com>
2026-06-20 20:32:28 +0530

    Added correct reference for vnet name

--- terraform-infra/module/compute/main.tf
+++ terraform-infra/module/compute/main.tf
@@ -7,7 +7,7 @@ resource "azurerm_public_ip" "public_ip" {
 }
 
 resource "azurerm_network_interface" "nic" {
-    name = "{var.vm_name}-nic"
+    name = "${var.vm_name}-nic"
     location = var.location
     resource_group_name = var.resource_group_name
 
